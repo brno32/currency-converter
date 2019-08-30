efb2905da23145060e1aab31813d2b5094544242
Alex Drozd <drozdster@gmail.com>
2019-08-29 22:57:11 -0500

    collects number of conversions in stats route

--- server/routes/stats.ts
+++ server/routes/stats.ts
@@ -1,12 +1,24 @@
 import express from "express";
 
+const { firebase_db } = require("../config");
+
 const router = express.Router();
+
+type QuerySnapshot = import("firebase").firestore.QuerySnapshot;
+type DocumentSnapshot = import("firebase").firestore.DocumentSnapshot;
 
 // @route    GET api/stats
 // @desc     Get statistics about this site
 // @access   Public
-router.get("/", (req, res) => {
-  res.send("Collect stats");
+router.get("/", async (req, res) => {
+  let documents = await firebase_db.collection("conversions").get();
+  documents.forEach((doc: DocumentSnapshot) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+  });
+
+  res.json({
+    total_conversions: documents.size
+  });
 });
 
 module.exports = router;
