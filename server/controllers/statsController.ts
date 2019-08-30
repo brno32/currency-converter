3b4803d889d64bc537fd27bb8a65ae4be605e0c2
Alex Drozd <drozdster@gmail.com>
2019-08-29 23:54:14 -0500

    separates logic for stats from router into controller

---
+++ server/controllers/statsController.ts
@@ -0,0 +1,32 @@
+import express from "express";
+
+const { firebase_db } = require("../config");
+
+type QuerySnapshot = import("firebase").firestore.QuerySnapshot;
+type DocumentSnapshot = import("firebase").firestore.DocumentSnapshot;
+
+const statsController = async (req: express.Request, res: express.Response) => {
+  let documents: QuerySnapshot = await firebase_db
+    .collection("conversions")
+    .get();
+
+  let destCurrs: { [currency: string]: number } = {};
+
+  documents.forEach((doc: DocumentSnapshot) => {
+    let data = doc.data();
+
+    if (data != undefined) destCurrs[data.to] = destCurrs[data.to] + 1 || 0;
+  });
+
+  let top_dest_currency: any = Object.keys(destCurrs).reduce((max, current) =>
+    destCurrs[max] > destCurrs[current] ? max : current
+  );
+
+  // TODO: add in total amount converted
+  res.json({
+    top_dest_currency: top_dest_currency,
+    total_conversions: documents.size
+  });
+};
+
+export default statsController;
--- server/routes/stats.ts
+++ server/routes/stats.ts
@@ -1,36 +1,12 @@
 import express from "express";
 
-const { firebase_db } = require("../config");
+import statsController from "../controllers/statsController";
 
 const router = express.Router();
-
-type QuerySnapshot = import("firebase").firestore.QuerySnapshot;
-type DocumentSnapshot = import("firebase").firestore.DocumentSnapshot;
 
 // @route    GET api/stats
 // @desc     Get statistics about this site
 // @access   Public
-router.get("/", async (req, res) => {
-  let documents: QuerySnapshot = await firebase_db
-    .collection("conversions")
-    .get();
-
-  let destCurrs: { [currency: string]: number } = {};
-
-  documents.forEach((doc: DocumentSnapshot) => {
-    let data = doc.data();
-
-    if (data != undefined) destCurrs[data.to] = destCurrs[data.to] + 1 || 0;
-  });
-
-  let top_dest_currency: any = Object.keys(destCurrs).reduce((max, current) =>
-    destCurrs[max] > destCurrs[current] ? max : current
-  );
-
-  res.json({
-    top_dest_currency: top_dest_currency,
-    total_conversions: documents.size
-  });
-});
+router.get("/", statsController);
 
 module.exports = router;
