9f309afd5986bddcaee0969b83a54a8da82985f8
Alex Drozd <drozdster@gmail.com>
2019-08-29 21:47:19 -0500

    adds firebase and logs requests to converter

--- server/config.ts
+++ server/config.ts
@@ -1,8 +1,24 @@
 import dotenv from "dotenv";
+import firebase from "firebase";
 
 dotenv.config();
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBKvx_wRRSceEWDnnuxi2dt2s5rCSYOQHo",
+  authDomain: "currency-converter-d49e5.firebaseapp.com",
+  databaseURL: "https://currency-converter-d49e5.firebaseio.com",
+  projectId: "currency-converter-d49e5",
+  storageBucket: "",
+  messagingSenderId: "343302309359",
+  appId: "1:343302309359:web:67d537c333647c95"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+
 module.exports = {
   api_key: process.env.API_KEY,
-  port: process.env.PORT
+  port: process.env.PORT,
+  firebase_db: db
 };
--- server/routes/convert.ts
+++ server/routes/convert.ts
@@ -1,11 +1,52 @@
 import express from "express";
+import {
+  query,
+  validationResult,
+  ValidationError,
+  Result
+} from "express-validator";
+
+const { firebase_db } = require("../config");
+
 const router = express.Router();
 
 // @route    GET api/convert
 // @desc     Convert a currency
 // @access   Public
-router.get("/", (req, res) => {
-  res.send("Convert a currency");
-});
+router.get(
+  "/",
+  [
+    query("amount", "amount is required")
+      .not()
+      .isEmpty(),
+    query("amount", "amount must be a number").isNumeric(),
+    query("from", "from is required")
+      .not()
+      .isEmpty(),
+    query("from", "from must be a string").isString(),
+    query("to", "to is required")
+      .not()
+      .isEmpty(),
+    query("to", "to must be a string").isString()
+  ],
+  (req: express.Request, res: express.Response) => {
+    const errors: Result<ValidationError> = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let amount: number = +req.query.amount; // cast to number
+    let from: string = req.query.from;
+    let to: string = req.query.to;
+
+    firebase_db.collection("conversions").add({
+      amount: amount,
+      from: from,
+      to: to
+    });
+
+    res.send("Convert a currency");
+  }
+);
 
 module.exports = router;
